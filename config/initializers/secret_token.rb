832c19639e6a1ca2f23208f5392327c277c99fa2
Emiliano Lozano <emiliano@minidonations.org>
2012-09-17 21:28:46 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KagWebsite::Application.config.secret_token = '93a25d26c2c98c0d484351a6b9aec0336958692ce5b45424cb52bae3f4b7b14b0677ba7cc01db6bd58475cf002a71518ad52b3af21bb795f75e7721970a2b930'
